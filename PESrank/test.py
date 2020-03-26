984a3649f0be45fa5831aee2cd6691e80a4a4579
lirondavid <47604157+lirondavid@users.noreply.github.com>
2020-03-26 21:27:23 +0200

    Add files via upload

--- PESrank/test.py
+++ PESrank/test.py
@@ -17,15 +17,15 @@ else:
 
     print(", according to this study, based on 905 million leaked passwords")
         
-    print("Your password is based on the leaked word: '"+str(explain[0][1])+ "' that was used by",int(float(explain[0][2])*n), "people")
+    print("Your password is based on the leaked word: '"+str(explain[0][0])+ "' that was used by",int(float(explain[0][1])*n), "people")
     for lst in explain:
-        if math.ceil(float(lst[2])*n)>=100:
+        if math.ceil(float(lst[1])*n)>=100:
             if lst[0]==1:
-                print("It uses a prefix that was used by",math.ceil(float(lst[2])*n), "people")
+                print("It uses a prefix that was used by",math.ceil(float(lst[1])*n), "people")
             if lst[0]==3:
-                print("It uses a suffix that was used by",math.ceil(float(lst[2])*n), "people")
+                print("It uses a suffix that was used by",math.ceil(float(lst[1])*n), "people")
             if lst[0]==4:
-                print("It uses a capitaliation pattern that was used by",math.ceil(float(lst[2])*n), "people")
+                print("It uses a capitaliation pattern that was used by",math.ceil(float(lst[1])*n), "people")
             if lst[0]==5:
-                print("It uses a l33t pattern that was used by",math.ceil(float(lst[2])*n), "people")
+                print("It uses a l33t pattern that was used by",math.ceil(float(lst[1])*n), "people")
 
